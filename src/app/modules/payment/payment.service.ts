131c02746d1fb16afd88c21dcd96c9f43737d704
Ruhul Amin <150836543+ruhulamin-et15@users.noreply.github.com>
2025-01-02 21:30:13 +0600

    update payment

--- src/app/modules/payment/payment.service.ts
+++ src/app/modules/payment/payment.service.ts
@@ -233,6 +233,7 @@ const getUserFromAuth0 = async (userEmail: string) => {
 
 const updateAuth0UserMetadata = async (userId: string, appMetadata: object) => {
   console.log(appMetadata);
+  console.log("userId:", userId);
   const tokenResponse = await axios.post(
     `https://${process.env.M2M_DOMAIN}/oauth/token`,
     {
