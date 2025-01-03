b404babc9827379e6f5fe91f2286701939792937
Ruhul Amin <150836543+ruhulamin-et15@users.noreply.github.com>
2025-01-03 11:38:13 +0600

    update last

--- src/app/modules/payment/payment.service.ts
+++ src/app/modules/payment/payment.service.ts
@@ -11,40 +11,6 @@ import bcrypt from "bcryptjs";
 
 dotenv.config({ path: path.join(process.cwd(), ".env") });
 
-const AUTH0_MANAGEMENT_API_TOKEN = "";
-const CONNECTION = "Username-Password-Authentication";
-
-interface Auth0User {
-  email: string;
-  connection: string;
-  password?: string;
-}
-
-const createUserInAuth0 = async (email: string): Promise<any> => {
-  const user: Auth0User = {
-    email,
-    connection: CONNECTION,
-  };
-
-  try {
-    const response = await axios.post(
-      `https://${process.env.AUTH0_DOMAIN}/api/v2/users`,
-      user,
-      {
-        headers: {
-          Authorization: `Bearer ${AUTH0_MANAGEMENT_API_TOKEN}`,
-        },
-      }
-    );
-
-    console.log("User created successfully in Auth0:", response.data);
-    return response.data;
-  } catch (error) {
-    console.error("Error creating user in Auth0:", error);
-    throw error;
-  }
-};
-
 const ROLE_GROUP_MAPPING: { [key: string]: string } = {
   rol_sXYkL5QJc63EvHJI: "360 Elite Crypto Trading Alerts",
   rol_kFz6E1TzYWKHnoNb: "360 Elite Stock Market Slayer",
@@ -62,8 +28,9 @@ const PRICE_ID_ROLE_MAPPING: { [key: string]: string } = {
 const stripePortalSessionInStripe = async (customerId: string) => {
   const session = await stripe.billingPortal.sessions.create({
     customer: customerId,
-    return_url: "https://boom360trader.com",
+    return_url: "https://360trader.com",
   });
+
   return session.url;
 };
 
@@ -139,33 +106,104 @@ const cancelSubscriptionInStripe = async (subscriptionId: string) => {
   return cancelSubcription;
 };
 
-const handleUserInAuth = async (event: Stripe.Event) => {
-  const subscription = event.data.object as Stripe.Subscription;
-  const customerId = subscription.customer as string;
+const handleUserInAuth = async (
+  event: Stripe.CustomerSubscriptionCreatedEvent
+) => {
+  const customerId = event.data.object.customer as string;
+  // const auth0Domain = process.env.M2M_DOMAIN;
+  // const auth0ClientId = process.env.M2M_CLIENT_ID;
+  // const auth0ClientSecret = process.env.M2M_CLIENT_SECRET;
 
   const customer = await stripe.customers.retrieve(customerId);
-  const email = (customer as any).email;
-
-  let user;
-  try {
-    user = await getUserFromAuth0(email);
-  } catch (error) {
-    console.log("User not found in Auth0. Creating user...");
-    user = await createUserInAuth0(email); // Ensure you have a createUserInAuth0 function
+  const userEmail = (customer as Stripe.Customer).email;
+  if (!userEmail) {
+    throw new ApiError(404, "Email not found for the given customer ID");
   }
 
-  await updateAuth0UserMetadata(user.user_id, {
-    priceId: subscription.items.data[0].price.id,
-    group:
-      ROLE_GROUP_MAPPING[
-        PRICE_ID_ROLE_MAPPING[subscription.items.data[0].price.id]
-      ],
-  });
+  const result = await validateAndAssignRole(userEmail);
+  return result;
 
-  await assignUserRole(
-    user.user_id,
-    PRICE_ID_ROLE_MAPPING[subscription.items.data[0].price.id]
-  );
+  // const getAuth0Token = async () => {
+  //   const tokenResponse = await axios.post(
+  //     `https://${auth0Domain}/oauth/token`,
+  //     {
+  //       client_id: auth0ClientId,
+  //       client_secret: auth0ClientSecret,
+  //       audience: `https://${auth0Domain}/api/v2/`,
+  //       grant_type: "client_credentials",
+  //       scope:
+  //         "read:users update:users create:user_tickets read:roles update:users_app_metadata",
+  //     }
+  //   );
+
+  //   const managementToken = tokenResponse.data.access_token;
+  //   return managementToken;
+  // };
+
+  // const managementToken = await getAuth0Token();
+
+  // // Get User by Email
+  // const userResponse = await axios.get(
+  //   `https://${auth0Domain}/api/v2/users-by-email?email=${userEmail}`,
+  //   {
+  //     headers: {
+  //       Authorization: `Bearer ${managementToken}`,
+  //     },
+  //   }
+  // );
+
+  // const users = userResponse.data;
+  // console.log(users);
+  // const userId = users[0]?.user_id;
+
+  // if (!userId) {
+  //   throw new ApiError(404, "User not found by email address");
+  // }
+
+  // const priceId = users[0]?.app_metadata?.priceId;
+
+  // let role: string | undefined;
+  // let groupName: string | undefined;
+
+  // if (priceId === "stockmarketslayer") {
+  //   role = "rol_kFz6E1TzYWKHnoNb";
+  //   groupName = "360 Elite Stock Market Slayer";
+  // } else if (priceId === "elitecryptoalerts") {
+  //   role = "rol_sXYkL5QJc6ЗEVHJ!";
+  //   groupName = "360 Elite Crypto Trading Alerts";
+  // }
+
+  // if (role && groupName) {
+  //   // Assign Role to User
+  //   await axios.post(
+  //     `https://${auth0Domain}/api/v2/users/${userId}/roles`,
+  //     {
+  //       roles: [role],
+  //     },
+  //     {
+  //       headers: {
+  //         Authorization: `Bearer ${managementToken}`,
+  //         "Content-Type": "application/json",
+  //       },
+  //     }
+  //   );
+  //   console.log(`✅ Role assigned to user ${userId} based on Price ID`);
+
+  //   // Assign User to Group
+  //   await axios.post(
+  //     `https://${auth0Domain}/api/v2/users/${userId}/groups`,
+  //     {
+  //       groups: [groupName],
+  //     },
+  //     {
+  //       headers: {
+  //         Authorization: `Bearer ${managementToken}`,
+  //         "Content-Type": "application/json",
+  //       },
+  //     }
+  //   );
+  //   console.log(`✅ Group ${groupName} assigned to user ${userId}`);
+  // }
 };
 
 const getUserFromAuth0 = async (userEmail: string) => {
@@ -182,7 +220,6 @@ const getUserFromAuth0 = async (userEmail: string) => {
   );
 
   const managementToken = tokenResponse.data.access_token;
-  console.log(managementToken);
   const userResponse = await axios.get(
     `https://${process.env.M2M_DOMAIN}/api/v2/users-by-email?email=${userEmail}`,
     {
@@ -276,37 +313,34 @@ const removeUserRole = async (userId: string, roleId: string) => {
   );
 };
 
-// Validate Subscription
 const validateAndAssignRole = async (userEmail: string) => {
   try {
     const user = await getUserFromAuth0(userEmail);
     if (!user) throw new ApiError(404, "User not found in Auth0");
 
-    const { stripe_customer_id, priceId } = user.app_metadata || {};
-
-    if (!stripe_customer_id || !priceId) {
-      await updateAuth0UserMetadata(user.user_id, {
-        priceId: null,
-        group: null,
-      });
-      console.log("Removed invalid metadata.");
+    const userInfo = await prisma.user.findUnique({
+      where: { email: userEmail },
+    });
+    if (!userInfo || !userInfo.subscriptionId || !userInfo.priceId) {
+      console.warn("No subscription details found in database for this user.");
       return { valid: false };
     }
 
     const subscriptions = await stripe.subscriptions.list({
-      customer: stripe_customer_id,
+      customer: userInfo.customerId as any,
       status: "active",
     });
 
     const validSubscription = subscriptions.data.find((sub) =>
-      sub.items.data.some((item) => item.price.id === priceId)
+      sub.items.data.some((item) => item.price.id === userInfo.priceId)
     );
 
     if (validSubscription) {
-      const roleId = PRICE_ID_ROLE_MAPPING[priceId];
+      const roleId = PRICE_ID_ROLE_MAPPING[userInfo.priceId];
       await assignUserRole(user.user_id, roleId);
       await updateAuth0UserMetadata(user.user_id, {
         group: ROLE_GROUP_MAPPING[roleId],
+        priceId: userInfo.priceId,
       });
       return { valid: true, group: ROLE_GROUP_MAPPING[roleId] };
     } else {
@@ -314,19 +348,16 @@ const validateAndAssignRole = async (userEmail: string) => {
         priceId: null,
         group: null,
       });
-      await removeUserRole(user.user_id, PRICE_ID_ROLE_MAPPING[priceId]);
+      await removeUserRole(
+        user.user_id,
+        PRICE_ID_ROLE_MAPPING[userInfo.priceId]
+      );
       return { valid: false };
     }
   } catch (error: any) {
     console.error("Validation error:", error.message);
     throw error;
   }
-};
-
-const handleSubscriptionDeleted = async (event: Stripe.Event) => {
-  event.data.object as Stripe.Subscription;
-
-  return;
 };
 
 const handleSubscriptionInAuth = async (userEmail: string) => {
@@ -422,6 +453,11 @@ const handleSubscriptionInAuth = async (userEmail: string) => {
 
 const handelPaymentWebhook = async (req: Request) => {
   const sig = req.headers["stripe-signature"] as string;
+
+  if (!sig) {
+    console.error("Missing Stripe signature header");
+  }
+
   let event: Stripe.Event;
 
   try {
@@ -430,15 +466,19 @@ const handelPaymentWebhook = async (req: Request) => {
       sig,
       config.stripe.webhook_secret as string
     );
+    let result;
+    switch (event.type) {
+      case "customer.subscription.created":
+        result = await handleUserInAuth(event);
+        break;
 
-    if (event.type === "customer.subscription.created") {
-      await handleUserInAuth(event);
-    } else if (event.type === "customer.subscription.deleted") {
-      await handleSubscriptionDeleted(event);
+      default:
+        break;
     }
+
+    return result;
   } catch (err: any) {
-    console.error("Webhook error:", err.message);
-    throw err;
+    return;
   }
 };
 
